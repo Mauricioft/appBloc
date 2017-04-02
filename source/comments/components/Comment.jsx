e0ef16d7eb82a61c279c92c25e12d4bd5bdbe058
Mauricio flor <Mauricio flor>
2017-04-02 15:52:45 -0500

    Init dependency

---
+++ source/comments/components/Comment.jsx
@@ -0,0 +1,32 @@
+import React, { PropTypes } from 'react';
+import { FormattedHTMLMessage } from 'react-intl';
+import styles from './Comment.css';
+
+function Comment(props) {
+  return (
+    <article id={`comment-${props.id}`} className={styles.comment}>
+      <div className={styles.meta}>
+        <FormattedHTMLMessage 
+          id="comment.meta.author" 
+          values={{
+            email: props.email,
+            name: props.name,
+          }}
+        />
+      </div>
+
+      <p className={styles.meta}>
+        {props.body}
+      </p>
+    </article>
+  );
+}
+
+Comment.propTypes = {
+  id: PropTypes.number.isRequired,
+  email: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+};
+
+export default Comment;
